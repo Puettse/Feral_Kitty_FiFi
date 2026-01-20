1c82d043f10a50f4d2a00d1c6b414dc399911f3a
Puettse <biguno.media@gmail.com>
2026-01-20 15:44:00 -0600

    Update main.py

--- Feral_Kitty_FiFi/main.py
+++ Feral_Kitty_FiFi/main.py
@@ -32,7 +32,7 @@ EXTENSIONS = [
     "Feral_Kitty_FiFi.features.scheduler",
     "Feral_Kitty_FiFi.features.channel_builder",
     "Feral_Kitty_FiFi.features.tickets_channels",
-    "Feral_Kitty_FiFi.features.profile_roles"
+    "Feral_Kitty_FiFi.features.profile_roles",
     "Feral_Kitty_FiFi.features.gimme_report"
 ]
 
