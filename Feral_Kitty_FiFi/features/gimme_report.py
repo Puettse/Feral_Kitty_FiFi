b425cf72957d68d68b8e3e5c76dfa641a6fb5e43
Puettse <biguno.media@gmail.com>
2026-01-20 16:05:56 -0600

    Update gimme_report.py

--- Feral_Kitty_FiFi/features/gimme_report.py
+++ Feral_Kitty_FiFi/features/gimme_report.py
@@ -593,3 +593,19 @@ async def setup(bot: commands.Bot):
             backfill_max_messages_per_channel=BACKFILL_MAX_MESSAGES_PER_CHANNEL,
         )
     )
+ # -----------
+ # Extension hook
+ # -----------
+ async def setup(bot: commands.Bot):
+-    bot.add_cog(
++    await bot.add_cog(
+         GimmeReport(
+             bot,
+             db_path=DB_PATH,
+             trigger=TRIGGER,
+             report_channel_id=REPORT_CHANNEL_ID,
+             join_leave_log_channel_ids=JOIN_LEAVE_LOG_CHANNEL_IDS,
+             ban_log_channel_id=BAN_LOG_CHANNEL_ID,
+             backfill_max_messages_per_channel=BACKFILL_MAX_MESSAGES_PER_CHANNEL,
+         )
+     )
